4b0c570e6284463659f07d2d6f53ca1cbc7d50db
Galih <gprayoga742@gmail.com>
2025-08-14 07:40:23 +0700

    update Seeder

--- app/Services/AirportsService.php
+++ app/Services/AirportsService.php
@@ -38,36 +38,4 @@ class AirportsService
         ];
     }
 
-    public function getAll()
-    {
-        $data = $this->airportsRepository->get();
-        return ResponseHelper::success(AirportsResource::collection($data), 'List bandara berhasil diambil');
-    }
-
-    public function store(AirportsRequest $request)
-    {
-        $payload = $this->mapStore($request);
-        $data = $this->airportsRepository->store($payload);
-        return ResponseHelper::success(new AirportsResource($data), 'Bandara berhasil ditambahkan');
-    }
-
-    public function show($id)
-    {
-        $data = $this->airportsRepository->show($id);
-        return ResponseHelper::success(new AirportsResource($data), 'Detail bandara berhasil diambil');
-    }
-
-    public function update(AirportsRequest $request, $id)
-    {
-        $airport = $this->airportsRepository->show($id); 
-        $payload = $this->mapUpdate($request->validated(), $airport);
-        $this->airportsRepository->update($id, $payload);
-        return ResponseHelper::success(null, 'Bandara berhasil diperbarui');
-    }
-
-    public function destroy($id)
-    {
-        $this->airportsRepository->delete($id);
-        return ResponseHelper::success(null, 'Bandara berhasil dihapus');
-    }
 }
